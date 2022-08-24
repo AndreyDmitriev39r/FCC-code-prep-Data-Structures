4257b84ca0c68459f00cb5a736c39043a6843805
Andrey Dmitriev <admit8490@gmail.com>
2022-08-24 04:15:13 +0200

    minor fix: text in typed array file

--- Typed Array.js
+++ Typed Array.js
@@ -1,9 +1,12 @@
-/* TASK
-Basically what happens in the background is that your browser will automatically give the right amount of memory space for that array. It will also change as needed if you add or remove data.
+/* THEORY
+Basically what happens in the background is that your browser will automatically give the right amount of memory space for that array.
+It will also change as needed if you add or remove data.
 
-However, in the world of high performance and different element types, sometimes you need to be more specific on how much memory is given to an array.
+However, in the world of high performance and different element types,
+sometimes you need to be more specific on how much memory is given to an array.
 
-Typed arrays are the answer to this problem. You are now able to say how much memory you want to give an array. Below is a basic overview of the different types of arrays available and the size in bytes for each element in that array.
+Typed arrays are the answer to this problem. You are now able to say how much memory you want to give an array.
+Below is a basic overview of the different types of arrays available and the size in bytes for each element in that array.
 
 Type	Each element size in bytes
 Int8Array	1
@@ -35,7 +38,7 @@ Buffers are general purpose objects that just carry data. You cannot access them
 
 Typed arrays do not have some of the methods traditional arrays have such as .pop() or .push(). Typed arrays also fail Array.isArray()
 
-THEORY
+TASK
 First create a buffer that is 64-bytes. Then create a Int32Array typed array with a view of it called i32View.
 */
 
